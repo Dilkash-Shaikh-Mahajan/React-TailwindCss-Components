8ea11c16f825db04af06e04bc1d4f2dc3c6cd71c
dilkash7 <dilkash.3mindsdigital@gmail.com>
2022-06-19 14:39:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Navbar from './Components/Navbar';
+import Analytics from './Components/Analytics';
+import Cards from './Components/Card';
+import Team from './Components/Team';
+import Blog from './Components/Blog';
+import Footer from './Components/Footer';
+export default function App() {
+	return (
+		<>
+			<Navbar />
+			<Analytics />
+			<Blog />
+			<Team />
+			<Cards />
+			<Footer />
+		</>
+	);
 }
 
-export default App;
